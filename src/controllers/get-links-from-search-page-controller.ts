9c5aa43af85fa923f0b8ba8cee9c067d74a85e4d
Artem Beresnev <artembert@gmail.com>
2020-01-03 17:55:47 +0200

    save links to json

--- src/controllers/get-links-from-search-page-controller.ts
+++ src/controllers/get-links-from-search-page-controller.ts
@@ -1,8 +1,10 @@
-import {markupPaths} from "../../config/paths";
-import {getLinksFromSearchPage} from "../compoenents/get-links-from-search-page";
+import { linksPath, markupPaths } from "../../config/paths";
+import { getLinksFromSearchPage } from "../components/get-links-from-search-page";
+import * as fs from "fs";
 
 getLinksFromSearchPageController();
 
 export function getLinksFromSearchPageController() {
-  console.log(getLinksFromSearchPage(markupPaths.path));
+  const links = getLinksFromSearchPage(markupPaths.path);
+  fs.writeFileSync(linksPath, JSON.stringify(links, undefined, 4));
 }
