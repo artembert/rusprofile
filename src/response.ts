96364e3ff713d3cd954be54102a3324607d99ee9
Artem Beresnev <artembert@gmail.com>
2020-01-03 17:58:17 +0200

    remove unused code

--- src/response.ts
+++ src/response.ts
@@ -3,8 +3,7 @@ import {writeMarkup} from "./write-markup";
 
 const url =
   "https://www.rusprofile.ru/search/6?query=%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%2C+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA&type=ul&search_inactive=2";
-const referer =
-  "https://www.rusprofile.ru/search/5?query=%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%2C+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA&type=ul&search_inactive=2";
+
 getPage();
 
 async function getPage() {
@@ -23,18 +22,10 @@ async function getPage() {
     })
     .set("Content-Type", "text/html")
     .send();
-    // .set({
-    //   referrer: referer,
-    //   referrerPolicy: "no-referrer-when-downgrade",
-    //   body: null,
-    //   mode: "cors"
-    // });
 
   if (response.ok) {
     console.log(response.text);
     writeMarkup(response.text.toString());
-    // let json = await response.body;
-    // console.log(json);
   } else {
     alert("Ошибка HTTP: " + response.status);
   }
