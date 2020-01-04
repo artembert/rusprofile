27854b4cf8d557f79702feb4e8d4f81545a138e6
Artem Beresnev <artembert@gmail.com>
2020-01-04 23:10:27 +0200

    remove unused code

--- index.ts
+++ index.ts
@@ -6,9 +6,6 @@ import * as fs from "fs";
 
 const pageNumber = 1;
 
-const url =
-  "https://www.rusprofile.ru/search?query=%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%2C+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA&type=ul&search_inactive=2";
-
 (async () => {
   const markupPath = getFilePath(markupPaths.kirovsk, pageNumber);
   const searchPageMarkup = await getPage(getUrl(pageNumber));
