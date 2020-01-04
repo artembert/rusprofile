cca03595c5f68d9e5a4203d0364ab41bb8ade0f7
Artem Beresnev <artembert@gmail.com>
2020-01-04 20:41:33 +0200

    optimise variables

--- index.ts
+++ index.ts
@@ -10,15 +10,11 @@ const url =
   "https://www.rusprofile.ru/search?query=%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%2C+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA&type=ul&search_inactive=2";
 
 (async () => {
+  const markupPath = getFilePath(markupPaths.kirovsk, pageNumber);
   const searchPageMarkup = await getPage(getUrl(pageNumber));
   if (searchPageMarkup) {
-    await writeMarkup(
-      getFilePath(markupPaths.kirovsk, pageNumber),
-      searchPageMarkup
-    );
-    const links = getLinksFromSearchPage(
-      getFilePath(markupPaths.kirovsk, pageNumber)
-    );
+    await writeMarkup(markupPath, searchPageMarkup);
+    const links = getLinksFromSearchPage(markupPath);
     fs.appendFileSync(linksPath, JSON.stringify(links, undefined, 4));
     console.log(`links saved to ${linksPath}`);
   } else {
