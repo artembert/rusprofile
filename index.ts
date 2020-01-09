a82262ebfa1854e93d898a3b1d846ab49e7fb4c3
Artem Beresnev <artembert@gmail.com>
2020-01-10 00:41:43 +0300

    add getDetailsFromKlerkController() to index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { downloadSearchPageMarkupController } from "./src/controllers/download-search-page-markup-controller";
 import { getLinksFromSearchPagesController } from "./src/controllers/get-links-from-search-page-controller";
 import { parseSearchPageController } from "./src/controllers/parse-search-page-controller";
+import { getDetailsFromKlerkController } from "./src/controllers/get-details-from-klerk";
 
 (async () => {
   // * Download search-pages markup
@@ -8,5 +9,7 @@ import { parseSearchPageController } from "./src/controllers/parse-search-page-c
   // * Get links from search-pages
   await getLinksFromSearchPagesController();
   // * Parse search-pages
-  await parseSearchPageController()
+  await parseSearchPageController();
+  // * Get details from Klerk
+  await getDetailsFromKlerkController();
 })();
