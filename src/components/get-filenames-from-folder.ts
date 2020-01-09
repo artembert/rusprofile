29cbcbc52ec20bd139a6f0f2ca74363e516d1500
Artem Beresnev <artembert@gmail.com>
2020-01-09 23:48:31 +0300

    getFilenamesFromFolder(): fix

--- src/components/get-filenames-from-folder.ts
+++ src/components/get-filenames-from-folder.ts
@@ -1,9 +1,8 @@
 import fs from "fs";
-import { markupFoldersPaths } from "../../config/paths";
 import { promisify } from "util";
 
 const readdir = promisify(fs.readdir);
 
 export async function getFilenamesFromFolder(folderPath: string) {
-  return await readdir(markupFoldersPaths.kirovsk);
+  return await readdir(folderPath);
 }
