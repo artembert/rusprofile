446f2eff724ea89632ac961c25bdec3148adfc3d
Artem Beresnev <artembert@gmail.com>
2020-01-11 16:09:13 +0300

    add request params for "shlisselburg"

--- config/rusprofile-query.ts
+++ config/rusprofile-query.ts
@@ -49,6 +49,15 @@ export const regionQuery: Map<Region, QueryProps[]> = new Map([
       }
     ],
 
+  ],
+  ["shlisselburg",
+    [
+      {
+        query:
+          "?query=%D0%9A%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%2C+%D1%88%D0%BB%D0%B8%D1%81%D1%81%D0%B5%D0%BB%D1%8C%D0%B1%D1%83%D1%80%D0%B3&type=ul&search_inactive=2",
+        pages: 2
+      }
+    ]
   ]
 ]);
 
--- config/session-variables.ts
+++ config/session-variables.ts
@@ -1,6 +1,6 @@
 import { Region } from "../src/types/region";
 import { QueryProps, regionQuery } from "./rusprofile-query";
 
-export const regionName: Region = "roshhino";
+export const regionName: Region = "shlisselburg";
 export const region: QueryProps = regionQuery.get(regionName)[0];
 export const pageNumber: number = 1;
